bb7938a7ac9330648ac7d505d0fadb1be67fb109
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2022-04-06 09:40:00 +0600

    test cases updates

--- test/register.test.js
+++ test/register.test.js
@@ -2,7 +2,7 @@ const {expect} = require('chai');
 
 describe('Register as an user', function(){
     it('should get an email', function(){
-        expect('ragib@email.com').to.be.eq('ragib@email.com')
+        expect('ragib@email.com').to.be.a('email');
     });
     it('should get a password', function(){
         expect('ragib@email.com').to.be.a('string');
